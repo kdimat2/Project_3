430bdf6c66af9b261e65a6fcc46ccf69e462d8ce
Katherine DiMattia <kdimat2@lsu.edu>
2024-10-28 13:56:58 -0500

    added html for enter screen

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
 // JS scripts placed here
-const element = document.getElementById('enter-button')
-const intro = document.getElementById('intro')
-
-element.addEventListener('click', () => {
-    intro.classList.add('fade-out')
-})
