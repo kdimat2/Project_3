55be25ff8a981007f653de8d632721cfb96e48c1
Katherine DiMattia <kdimat2@lsu.edu>
2024-10-28 12:41:15 -0500

    trying to figure out enter page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 // JS scripts placed here
+const element = document.getElementById('enter-button')
+const intro = document.getElementById('intro')
+
+element.addEventListener('click', () => {
+    intro.classList.add('fade-out')
+})
