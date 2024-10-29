9a314fdb098616cf8090dd94dec3fb5b1de123d6
Katherine DiMattia <kdimat2@lsu.edu>
2024-10-29 15:56:50 -0500

    jason added java script and it was soooo cool

--- js/main.js
+++ js/main.js
@@ -4,4 +4,7 @@ const intro = document.getElementById('intro');
 const page = document.body;
 
 console.log(page);
-element.addEventListener('click
+element.addEventListener('click', () => {
+    page.classList.add('entered');
+    intro.classList.add('fade-out');
+})
